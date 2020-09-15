1d133da1e49586ef55ba2446722490bd9b85f1f1
Alexandre Amorim <ajoluam@hotmail.com>
2020-09-15 10:50:44 -0300

    Ajustes de implementacao para post

--- src/main/java/com/example/grafana/prometheus/demo/controller/TestController.java
+++ src/main/java/com/example/grafana/prometheus/demo/controller/TestController.java
@@ -4,7 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.grafana.prometheus.demo.utils.MicrometerService.MicrometerService;
@@ -15,17 +16,24 @@ public class TestController {
 	@Autowired
 	private MicrometerService micrometerService;
 	
-	@GetMapping("/message")
-	public String teste() {
-		Map<String, String> teste1 = new HashMap<>();
-		teste1.put("id_parceiro", "111111");
-		teste1.put("id_pessoa", "ID_PESSOA1");
-		teste1.put("id_produto", "ID_PRODUTO1");
-		teste1.put("id_proposta", "1111");
-		teste1.put("numero_loja", "000000000001");
-		teste1.put("numero_operador", "111.111.111-11");
+	@PostMapping("/message")
+	public String teste(
+			@RequestParam("id_parceiro") String parceiro,
+			@RequestParam("id_pessoa") String pessoa,
+			@RequestParam("id_produto") String produto,
+			@RequestParam("id_proposta") String proposta,
+			@RequestParam("numero_loja") String loja,
+			@RequestParam("numero_operador") String operador
+			) {
+		Map<String, String> mapa = new HashMap<>();
+		mapa.put("id_parceiro", parceiro);
+		mapa.put("id_pessoa", pessoa);
+		mapa.put("id_produto", produto);
+		mapa.put("id_proposta", proposta);
+		mapa.put("numero_loja", loja);
+		mapa.put("numero_operador", operador);
 		
-		micrometerService.logMetrics(teste1);
+		micrometerService.logMetrics(mapa);
 		
 			
 		
